289d07b176471e611ef195ddb93843e6538bf77e
zhq <32982289+zhq1@users.noreply.github.com>
2018-09-24 14:09:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,14 +44,17 @@ displayErr() {
     
     
     # update package and upgrade Ubuntu
-    sudo apt-get update --fix-missing
-    sudo apt-get autoremove 
-    sudo apt-get clean 
-    sudo apt-get install -f
+    sudo apt-get -y autoremove git
+    sudo mv /var/lib/dpkg/info /var/lib/dpkg/info.bak
+    sudo mkdir /var/lib/dpkg/info
+    sudo apt-get update
+    sudo apt-get -y dist-upgrade
+    sudo mv /var/lib/dpkg/info/* /var/lib/dpkg/info.bak
+    sudo rm -rf /var/lib/dpkg/info
+    sudo mv /var/lib/dpkg/info.bak /var/lib/dpkg/info
     sudo apt-get -y update 
     sudo apt-get -y dist-upgrade
     sudo apt-get -y upgrade
-    sudo apt-get -y autoremove git
     
     output " "
     output "Switching to Aptitude"
