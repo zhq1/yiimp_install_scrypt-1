f711b99efb1a1c04a42eb0d71e0ca4e0d9090987
zhq <32982289+zhq1@users.noreply.github.com>
2018-09-24 12:16:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ displayErr() {
     
     # update package and upgrade Ubuntu
     sudo apt-get -y update 
+    sudo apt-get -y dist-upgrade
     sudo apt-get -y upgrade
     sudo apt-get -y autoremove
     
